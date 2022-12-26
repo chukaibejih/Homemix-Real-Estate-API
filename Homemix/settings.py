eefecf4f1b0f2df7359ca1fdf6aa0762373077b8
Ibejih <chukaibejih@gmail.com>
2022-12-26 12:27:16 +0100

    allowed all hosts

--- Homemix/settings.py
+++ Homemix/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
