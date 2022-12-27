1b55a7296c56ea4271d0fc0d4d40219f3bb78a21
Ibejih <chukaibejih@gmail.com>
2022-12-27 18:04:57 +0100

    refactored code

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errexit
 
-pipenv install
-pipenv shell
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
