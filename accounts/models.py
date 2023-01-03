ac19a1a300416eca2eac9b42ddf89a1828024d5b
Ibejih <chukaibejih@gmail.com>
2023-01-03 22:42:37 +0100

    property models

--- accounts/models.py
+++ accounts/models.py
@@ -42,23 +42,23 @@ class User(AbstractUser, PermissionsMixin):
         return self.email
 
 
-# class Property(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="properties")
-#     address = models.CharField(max_length=200)
-#     city = models.CharField(max_length=100)
-#     state = models.CharField(max_length=100)
-#     zip_code = models.CharField(max_length=20)
-#     property_type = models.CharField(max_length=50, choices=PROPERTY_TYPE_CHOICES)
-#     price = models.DecimalField(max_digits=10, decimal_places=2)
-#     beds = models.PositiveIntegerField()
-#     baths = models.DecimalField(max_digits=10, decimal_places=1)
-#     description = models.TextField(blank=True)
-#     image_urls = models.TextField(blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Property(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="properties")
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    property_type = models.CharField(max_length=50, choices=PROPERTY_TYPE_CHOICES)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    beds = models.PositiveIntegerField()
+    baths = models.DecimalField(max_digits=10, decimal_places=1)
+    description = models.TextField(blank=True)
+    image_urls = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return f"{self.address}, {self.city}, {self.state}"
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state}"
 
 
