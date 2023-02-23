44f7c5985409ae7c582088646c8c944a3738b50d
Ibejih <chukaibejih@gmail.com>
2023-02-23 12:43:09 +0100

    API intro

--- Homemix/urls.py
+++ Homemix/urls.py
@@ -24,10 +24,15 @@ schema_view = swagger_get_schema_view(
    openapi.Info(
         title="Homemix Real Estate API",
         default_version='1.0',
-        description='''This real estate API allows users to search for properties with information 
-                        about the property type, address, price, size, number of bedrooms and bathrooms, 
-                        description, photos, availability, and contact information. The API is built using 
-                        Python, Django and Django Rest Framework.''',
+        description='''Welcome to Homemix, a powerful and flexible Real Estate API that enables users to buy and sell properties with ease. Homemix is a RESTful API that provides endpoints for CRUD operations on real estate listings, making it simple for users to create, read, update, and delete listings.
+
+Homemix offers a comprehensive solution for buying and selling properties, with advanced search and filtering capabilities, secure authentication, and support for multiple media types. With detailed information on property type, address, price, size, number of bedrooms and bathrooms, description, photos, availability, and contact information, Homemix makes it easy for potential buyers to search for properties that meet their specific needs and requirements.
+
+In addition to these features, Homemix now also includes a referral system, providing a unique way for users to invite others to the platform and receive rewards or incentives. Homemix is built with Django Rest Framework (DRF), a powerful and flexible framework for building APIs in Django. This makes it easy to extend and customize the API to meet specific needs, and provides comprehensive documentation and examples to get you started quickly.
+
+With Homemix, you can register as either a buyer or seller, and access secure authentication and authorization through the use of token-based authentication. This ensures that only authorized users have access to sensitive information and the ability to manipulate data.
+
+Homemix is the ideal solution for anyone looking to buy or sell properties with ease, offering a user-friendly interface and advanced features to make the process as smooth and efficient as possible. This documentation will provide a technical overview of the platform, including the technologies used to gather and display pricing data, and how to interact with the API to perform CRUD operations on real estate listings.''',
         contact=openapi.Contact(name="Chukwuka Ibejih", email="chukaibejih@gmail.com"),
         license=openapi.License(name="MIT license"),
    ),
